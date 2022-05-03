2d305a26812d3f11839fcd5f76cb5570db9a5fd7
dboures <dboures@att.net>
2022-05-03 18:07:17 -0500

    1/2 of POC

--- programs/linked-list/src/lib.rs
+++ programs/linked-list/src/lib.rs
@@ -7,9 +7,59 @@ pub mod linked_list {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        ctx.accounts.node.value = 1;
+        ctx.accounts.node.next_node = Pubkey::default();
+        Ok(())
+    }
+
+    pub fn add_node(ctx: Context<AddNode>) -> Result<()> {
+        ctx.accounts.new_node.value = ctx.accounts.old_node.value + 1;
+        ctx.accounts.new_node.next_node = Pubkey::default();
+
+        ctx.accounts.old_node.next_node = ctx.accounts.new_node.key();
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    /// CHECK: I don't care
+    #[account(signer, mut)]
+    pub signer: AccountInfo<'info>,
+    /// CHECK: I don't care
+    #[account(init, 
+        seeds = [signer.key.as_ref()], 
+        bump, 
+        payer = signer,
+        space = 8 + 32 + 1)]
+    pub node: Account<'info, Node>,
+    pub system_program: Program<'info, System>,
+}
+
+
+#[derive(Accounts)]
+pub struct AddNode<'info> {
+    /// CHECK: I don't care
+    #[account(signer, mut)]
+    pub signer: AccountInfo<'info>,
+    /// CHECK: I don't care
+    #[account(mut)]
+    pub old_node: Account<'info, Node>,
+    /// CHECK: I don't care
+    #[account(init,
+        payer = signer,
+        space = 8 + 32 + 1)]
+    pub new_node: Account<'info, Node>,
+    pub system_program: Program<'info, System>,
+}
+
+/// Node Account for linked list
+#[account]
+#[derive(Copy)]
+pub struct Node {
+    /// Node value of linked list
+    pub value: u8, // 1 byte
+
+    // start liquidity
+    pub next_node: Pubkey, // 32 bytes
+}
--- tests/linked-list.ts
+++ tests/linked-list.ts
@@ -1,16 +1,79 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { LinkedList } from "../target/types/linked_list";
+import {
+  PublicKey,
+  Keypair,
+  SystemProgram
+} from "@solana/web3.js";
+import NodeWallet from "@project-serum/anchor/dist/cjs/nodewallet";
+const assert = require("assert");
 
 describe("linked-list", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const baseProvider = anchor.AnchorProvider.local();
+  const TEST_PAYER = Keypair.fromSecretKey(
+    (baseProvider.wallet as NodeWallet).payer.secretKey
+  );
 
   const program = anchor.workspace.LinkedList as Program<LinkedList>;
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const [nodeAddress, bump] = await PublicKey.findProgramAddress(
+      [baseProvider.wallet.publicKey.toBytes()],
+      program.programId
+    );
+
+    const tx = await program.rpc.initialize(
+      {
+        accounts: {
+          node: nodeAddress,
+          signer: baseProvider.wallet.publicKey,
+          systemProgram: SystemProgram.programId,
+        },
+        signers: [TEST_PAYER],
+      }
+    );
+
+    const node = await program.account.node.fetch(nodeAddress)
+    assert.ok(node.value == 1);
+    assert.ok(node.nextNode.equals(PublicKey.default));
   });
+
+  it("It can add node!", async () => {
+    const [oldAddress, bump] = await PublicKey.findProgramAddress(
+      [baseProvider.wallet.publicKey.toBytes()],
+      program.programId
+    );
+
+    const newNodeKp = Keypair.generate();
+
+    const tx = await program.rpc.addNode(
+      {
+        accounts: {
+          oldNode: oldAddress,
+          newNode: newNodeKp.publicKey,
+          signer: baseProvider.wallet.publicKey,
+          systemProgram: SystemProgram.programId,
+        },
+        signers: [TEST_PAYER, newNodeKp], // HAVE TO think about who owns these, how to keep signatures valid, obviously it should be PDAs but hmm
+      }
+    );
+
+    const prevNode = await program.account.node.fetch(oldAddress)
+    console.log(prevNode)
+    assert.ok(prevNode.value == 1);
+    assert.ok(prevNode.nextNode.equals(newNodeKp.publicKey));
+
+    const newNode = await program.account.node.fetch(newNodeKp.publicKey)
+    console.log(newNode)
+  });
+
+
+  // it("It can add a lot of nodes", async () => {
+  // });
+
+  // it("It can read a lot of nodes", async () => {
+  // });
+
 });
