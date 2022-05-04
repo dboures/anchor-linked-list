f427e6823713c386c93b0058589c59829882bc41
dboures <dboures@att.net>
2022-05-04 00:45:53 -0500

    2/2 POC (use zero copy)

--- programs/linked-list/src/lib.rs
+++ programs/linked-list/src/lib.rs
@@ -6,17 +6,52 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod linked_list {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        ctx.accounts.node.value = 1;
-        ctx.accounts.node.next_node = Pubkey::default();
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
 
     pub fn add_node(ctx: Context<AddNode>) -> Result<()> {
-        ctx.accounts.new_node.value = ctx.accounts.old_node.value + 1;
-        ctx.accounts.new_node.next_node = Pubkey::default();
+        let node_stack = &mut ctx.accounts.stack.load_mut()?; // Necessary to load zero_copy accounts
 
-        ctx.accounts.old_node.next_node = ctx.accounts.new_node.key();
+        if node_stack.idx >= 749 {
+            panic!("Stack is full, better empty it out");
+        }
+
+        let index = node_stack.idx.clone();
+        node_stack.nodes[index as usize] = Node {
+            value: 21,
+            foo: Pubkey::default(),
+            bar: Pubkey::default(),
+            baz: Pubkey::default(),
+            ser: Pubkey::default(),
+        };
+
+        node_stack.idx += 1;
+
+        Ok(())
+    }
+
+    pub fn iterate(ctx: Context<Iterate>) -> Result<()> {
+        let node_stack = &mut ctx.accounts.stack.load()?; // Necessary to load zero_copy accounts
+
+        let mut account_keys = Vec::new();
+
+        for n in 0..200 { // 200 works
+            account_keys.push(node_stack.nodes[n].foo)
+        }
+
+        Ok(())
+    }
+
+    pub fn break_stack(ctx: Context<Iterate>) -> Result<()> {
+        let node_stack = &mut ctx.accounts.stack.load()?; // Necessary to load zero_copy accounts
+
+        let mut account_keys = Vec::new();
+
+        for n in 0..300 { // 300 is too much
+            account_keys.push(node_stack.nodes[n].foo)
+        }
+
         Ok(())
     }
 }
@@ -27,39 +62,75 @@ pub struct Initialize<'info> {
     #[account(signer, mut)]
     pub signer: AccountInfo<'info>,
     /// CHECK: I don't care
-    #[account(init, 
-        seeds = [signer.key.as_ref()], 
-        bump, 
-        payer = signer,
-        space = 8 + 32 + 1)]
-    pub node: Account<'info, Node>,
-    pub system_program: Program<'info, System>,
+    #[account(zero)]
+    stack: AccountLoader<'info, NodeStack>,
 }
-
 
 #[derive(Accounts)]
 pub struct AddNode<'info> {
     /// CHECK: I don't care
-    #[account(signer, mut)]
+    #[account(signer)]
     pub signer: AccountInfo<'info>,
+
     /// CHECK: I don't care
     #[account(mut)]
-    pub old_node: Account<'info, Node>,
+    stack: AccountLoader<'info, NodeStack>,
+}
+
+#[derive(Accounts)]
+pub struct Iterate<'info> {
     /// CHECK: I don't care
-    #[account(init,
-        payer = signer,
-        space = 8 + 32 + 1)]
-    pub new_node: Account<'info, Node>,
-    pub system_program: Program<'info, System>,
+    #[account(signer)]
+    pub signer: AccountInfo<'info>,
+    /// CHECK: I don't care
+    stack: AccountLoader<'info, NodeStack>,
 }
 
-/// Node Account for linked list
-#[account]
-#[derive(Copy)]
-pub struct Node {
-    /// Node value of linked list
+#[account(zero_copy)]
+pub struct NodeStack {
+    pub nodes: [Node; 750], // Max account size is 10 MB (1_000_000 bytes), here we have 750 * 129 = 967_500
+    pub idx: u16,
+}
+
+/// Node Struct for array
+#[zero_copy]
+pub struct Node { // 129 bytes total
     pub value: u8, // 1 byte
-
-    // start liquidity
-    pub next_node: Pubkey, // 32 bytes
+    pub foo: Pubkey, // 32
+    pub bar: Pubkey, // 32
+    pub baz: Pubkey, // 32
+    pub ser: Pubkey, // 32
 }
+
+// A separate type is used for the RPC interface for two main reasons.
+//
+// 1. AnchorSerialize and AnchorDeserialize must be derived. Anchor requires
+//    *all* instructions to implement the AnchorSerialize and AnchorDeserialize
+//    traits, so any types in method signatures must as well.
+// 2. All types for zero copy deserialization are `#[repr(packed)]`. However,
+//    the implementation of AnchorSerialize (i.e. borsh), uses references
+//    to the fields it serializes. So if we were to just throw tehse derives
+//    onto the other `Event` struct, we would have references to
+//    `#[repr(packed)]` fields, which is unsafe. To avoid the unsafeness, we
+//    just use a separate type.
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct RpcNode {
+    pub value: u8,
+    pub foo: Pubkey,
+    pub bar: Pubkey, // 32
+    pub baz: Pubkey, // 32
+    pub ser: Pubkey, // 32
+}
+
+impl From<RpcNode> for Node {
+    fn from(e: RpcNode) -> Node {
+        Node {
+            value: e.value,
+            foo: e.foo,
+            bar: e.bar,
+            baz: e.baz,
+            ser: e.ser,
+        }
+    }
+}
+
--- tests/linked-list.ts
+++ tests/linked-list.ts
@@ -10,70 +10,100 @@ import NodeWallet from "@project-serum/anchor/dist/cjs/nodewallet";
 const assert = require("assert");
 
 describe("linked-list", () => {
-  // Configure the client to use the local cluster.
-  const baseProvider = anchor.AnchorProvider.local();
-  const TEST_PAYER = Keypair.fromSecretKey(
-    (baseProvider.wallet as NodeWallet).payer.secretKey
-  );
 
-  const program = anchor.workspace.LinkedList as Program<LinkedList>;
+  let program: any;
+  let baseProvider: anchor.AnchorProvider;
+  let TEST_PAYER: Keypair;
 
-  it("Is initialized!", async () => {
-    const [nodeAddress, bump] = await PublicKey.findProgramAddress(
-      [baseProvider.wallet.publicKey.toBytes()],
-      program.programId
+  let nodeStack: Keypair;
+  // let bump: number;
+  // let secondNodeKp: Keypair;
+
+  before(async () => {
+
+    baseProvider = anchor.AnchorProvider.local();
+    TEST_PAYER = Keypair.fromSecretKey(
+      (baseProvider.wallet as NodeWallet).payer.secretKey
     );
+  
+    program = anchor.workspace.LinkedList as Program<LinkedList>;
+  
+    // [stack, bump] = await PublicKey.findProgramAddress(
+    //   [baseProvider.wallet.publicKey.toBytes()],
+    //   program.programId
+    // );
+
+    nodeStack = Keypair.generate()
+
+  })
+
+  it("It can initialize zero copy account", async () => {
+
+    const size = 967502 + 8; // Account size in bytes.
 
     const tx = await program.rpc.initialize(
       {
         accounts: {
-          node: nodeAddress,
+          stack: nodeStack.publicKey,
           signer: baseProvider.wallet.publicKey,
           systemProgram: SystemProgram.programId,
+        },
+        instructions: [await program.account.nodeStack.createInstruction(nodeStack, size)],
+        signers: [TEST_PAYER, nodeStack],
+      }
+    );
+
+    const stackAccount = await program.account.nodeStack.fetch(nodeStack.publicKey);
+    assert.ok(stackAccount.nodes.length == 750);
+    assert.ok(stackAccount.idx == 0);
+  });
+
+  it("It can add a node", async () => {
+    const tx = await program.rpc.addNode(
+      {
+        accounts: {
+          stack: nodeStack.publicKey,
+          signer: baseProvider.wallet.publicKey,
         },
         signers: [TEST_PAYER],
       }
     );
 
-    const node = await program.account.node.fetch(nodeAddress)
-    assert.ok(node.value == 1);
-    assert.ok(node.nextNode.equals(PublicKey.default));
+    const stackAccount = await program.account.nodeStack.fetch(nodeStack.publicKey)
+    const newNode = stackAccount.nodes[stackAccount.idx - 1];
+
+    assert.ok(stackAccount.nodes.length == 750);
+    assert.ok(stackAccount.idx == 1);
+    assert.ok(newNode.value == 21);
+    assert.ok(newNode.foo.equals(PublicKey.default));
   });
 
-  it("It can add node!", async () => {
-    const [oldAddress, bump] = await PublicKey.findProgramAddress(
-      [baseProvider.wallet.publicKey.toBytes()],
-      program.programId
-    );
-
-    const newNodeKp = Keypair.generate();
-
-    const tx = await program.rpc.addNode(
+  it("It will allow us to iterate over accounts", async () => {
+    const tx = await program.rpc.iterate(
       {
         accounts: {
-          oldNode: oldAddress,
-          newNode: newNodeKp.publicKey,
+          stack: nodeStack.publicKey,
           signer: baseProvider.wallet.publicKey,
-          systemProgram: SystemProgram.programId,
         },
-        signers: [TEST_PAYER, newNodeKp], // HAVE TO think about who owns these, how to keep signatures valid, obviously it should be PDAs but hmm
+        signers: [TEST_PAYER],
       }
     );
-
-    const prevNode = await program.account.node.fetch(oldAddress)
-    console.log(prevNode)
-    assert.ok(prevNode.value == 1);
-    assert.ok(prevNode.nextNode.equals(newNodeKp.publicKey));
-
-    const newNode = await program.account.node.fetch(newNodeKp.publicKey)
-    console.log(newNode)
   });
-
-
-  // it("It can add a lot of nodes", async () => {
-  // });
-
-  // it("It can read a lot of nodes", async () => {
-  // });
+    
+    it("It will break (unpredictably) if we iterate over too many accounts", async () => { // something to think about
+      try {
+        const tx = await program.rpc.breakStack(
+          {
+            accounts: {
+              stack: nodeStack.publicKey,
+              signer: baseProvider.wallet.publicKey,
+            },
+            signers: [TEST_PAYER],
+          }
+        );
+      } catch (error) {
+        console.log(error)
+      }
+    });
 
 });
